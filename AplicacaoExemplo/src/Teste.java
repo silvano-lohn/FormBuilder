5f6324cdb88d08261e7e6fa7d78bb7975f1609ce
Silvano Lohn <silvano.lohn@gmail.com>
2012-10-17 22:40:35 -0300

    Update AplicacaoExemplo/src/Teste.java
    teste

--- AplicacaoExemplo/src/Teste.java
+++ AplicacaoExemplo/src/Teste.java
@@ -1,4 +1,13 @@
 
+@Model({
+	@Component(label="Label do Campo:", type=ComponentType.INPUT, attribute="atributoDaClasseDeModelo"),
+	@Component(label="Label do Campo:", type=ComponentType.CHECKBOX, attribute="atributoDaClasseDeModelo")
+})
+ClasseDeModelo classeDeModelo;
+
+
+
+
 @Component(label="Label do Campo:", type=ComponentType.INPUT)
 String campoDaClasseDeControle1;
 @Component(label="Label do Campo:", type=ComponentType.CHECKBOX)
